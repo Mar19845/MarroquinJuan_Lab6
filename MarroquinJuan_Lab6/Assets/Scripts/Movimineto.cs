159eb12f744ac93eb8f6c3e0e18dfd70a0ebfb09
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2020-05-10 14:43:11 -0600

    agregar movimineto y colliders

---
+++ MarroquinJuan_Lab6/Assets/Scripts/Movimineto.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimineto : MonoBehaviour
+{
+    private Rigidbody2D Rgb2d;
+    public float num = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rgb2d = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newScale = transform.localScale;
+        if(Input.GetAxis("Horizontal")> 0.0f){
+            newScale.x = -5.0f;
+        }
+        else if(Input.GetAxis("Horizontal") < 0.0f){
+             newScale.x = 5.0f;
+        } 
+    }
+    private void FixedUpdate()
+    {
+        if(Rgb2d){
+            Rgb2d.AddForce(new Vector2(Input.GetAxis("Horizontal")* num ,0));
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("PowerUp"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
