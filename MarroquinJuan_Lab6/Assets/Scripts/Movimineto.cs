2687a53dddcadb4aa97f52015de106564fe4dd82
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2020-05-10 15:33:42 -0600

    agregar salto y voltearse al personaje

--- MarroquinJuan_Lab6/Assets/Scripts/Movimineto.cs
+++ MarroquinJuan_Lab6/Assets/Scripts/Movimineto.cs
@@ -6,8 +6,7 @@ public class Movimineto : MonoBehaviour
 {
     private Rigidbody2D Rgb2d;
     public float num = 5;
-
-
+    public float velocidad = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,13 +17,18 @@ public class Movimineto : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump();
+        }
         Vector3 newScale = transform.localScale;
         if(Input.GetAxis("Horizontal")> 0.0f){
-            newScale.x = -5.0f;
+            newScale.x = -1.0f;
         }
         else if(Input.GetAxis("Horizontal") < 0.0f){
-             newScale.x = 5.0f;
+             newScale.x = 1.0f;
         } 
+        transform.localScale = newScale;
     }
     private void FixedUpdate()
     {
@@ -38,5 +42,16 @@ public class Movimineto : MonoBehaviour
         {
             Destroy(other.gameObject);
         }
+    }
+    void jump()
+    {
+        if(Rgb2d)
+        {
+            if(Mathf.Abs(Rgb2d.velocity.y)< 0.05f)
+            {
+                Rgb2d.AddForce(new Vector2(0,velocidad), ForceMode2D.Impulse);
+            }
+        }
+
     }
 }
