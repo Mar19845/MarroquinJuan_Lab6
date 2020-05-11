9de5936e63d28f53302ed9296d95af90e7f595ca
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2020-05-10 20:23:40 -0600

    agregue inmuidad al comerse el hongo y se muere con el enemigo

--- MarroquinJuan_Lab6/Assets/Scripts/Movimineto.cs
+++ MarroquinJuan_Lab6/Assets/Scripts/Movimineto.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Movimineto : MonoBehaviour
 {
     private Rigidbody2D Rgb2d;
-    public float num = 5;
-    public float velocidad = 5;
+    public float velocidadCaminar = 5;
+    public float velocidadSalto = 5;
+    private bool powerUp;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +18,7 @@ public class Movimineto : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Jump"))
+        if((Input.GetKeyDown(KeyCode.W) || Input.GetButtonDown("Jump")))
         {
             jump();
         }
@@ -33,15 +34,33 @@ public class Movimineto : MonoBehaviour
     private void FixedUpdate()
     {
         if(Rgb2d){
-            Rgb2d.AddForce(new Vector2(Input.GetAxis("Horizontal")* num ,0));
+            Rgb2d.AddForce(new Vector2(Input.GetAxis("Horizontal")* velocidadCaminar ,0));
         }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
+        // para coger el hongo
         if(other.CompareTag("PowerUp"))
         {
+            powerUp = true;
             Destroy(other.gameObject);
         }
+        // para matar al enemigo
+        {
+         if (other.gameObject.CompareTag("Malo")) 
+         {
+             if (powerUp)
+             {
+                 Destroy(other.gameObject);
+             }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+        
     }
     void jump()
     {
@@ -49,7 +68,7 @@ public class Movimineto : MonoBehaviour
         {
             if(Mathf.Abs(Rgb2d.velocity.y)< 0.05f)
             {
-                Rgb2d.AddForce(new Vector2(0,velocidad), ForceMode2D.Impulse);
+                Rgb2d.AddForce(new Vector2(0,velocidadSalto), ForceMode2D.Impulse);
             }
         }
 
